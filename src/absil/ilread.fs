dc92bbdf4914525d14c08b43cfccbe277c30eb61
TIHan <lol.tihan@gmail.com>
2018-11-09 15:48:52 -0800

    Progress

--- src/absil/ilread.fs
+++ src/absil/ilread.fs
@@ -48,6 +48,193 @@ let doubleOfBits (x:int64) = System.BitConverter.Int64BitsToDouble(x)
 //---------------------------------------------------------------------
 // Utilities.  
 //---------------------------------------------------------------------
+    
+module rec ILReader = 
+
+    //type SignatureTypeProvider() =
+    //    interface ISignatureTypeProvider<ILType, int> with
+
+    //        member __.GetFunctionPointerType(sign) = 
+    //            {
+    //                CallingConv = ILCallingConv.Callconv(ILThisConvention.Static, ILArgConvention.Default)
+    //                ArgTypes = 
+    //            }
+    //            ILType.FunctionPointer(ILCallingSignature)
+
+    let rec convertAssemblyReferenceToILAssemblyRef (mdReader: System.Reflection.Metadata.MetadataReader) (asmRef: AssemblyReference) =
+        let name = mdReader.GetString(asmRef.Name)
+        let flags = asmRef.Flags
+
+        let hash = 
+            let hashValue = asmRef.HashValue
+            if hashValue.IsNil then None
+            else Some(mdReader.GetBlobBytes(hashValue))
+
+        let publicKey =
+            if asmRef.PublicKeyOrToken.IsNil then None
+            else 
+                let bytes = mdReader.GetBlobBytes(asmRef.PublicKeyOrToken)
+                let publicKey = 
+                    if int (flags &&& AssemblyFlags.PublicKey) <> 0 then
+                        PublicKey(bytes)
+                    else
+                        PublicKeyToken(bytes)
+                Some(publicKey)
+
+        let retargetable = int (flags &&& AssemblyFlags.Retargetable) <> 0
+
+        let v = asmRef.Version
+        let version = Some(uint16 v.Major, uint16 v.Minor, uint16 v.Build, uint16 v.Revision)
+
+        let locale =
+            let locale = mdReader.GetString(asmRef.Culture)
+            if String.IsNullOrWhiteSpace(locale) then None
+            else Some(locale)
+
+        ILAssemblyRef.Create(name, hash, publicKey, retargetable, version, locale)
+
+    let rec convertModuleReferenceToILModuleRef (mdReader: System.Reflection.Metadata.MetadataReader) (modRef: ModuleReference) =
+        let name = mdReader.GetString(modRef.Name)
+        ILModuleRef.Create(name, hasMetadata = true, hash = None)
+
+    let rec convertTypeReferenceToILTypeRef (mdReader: System.Reflection.Metadata.MetadataReader) (typeRef: TypeReference) =
+        let ilScopeRef =
+            match typeRef.ResolutionScope.Kind with
+            | HandleKind.AssemblyReference ->
+                let asmRef = mdReader.GetAssemblyReference(AssemblyReferenceHandle.op_Explicit(typeRef.ResolutionScope))
+                ILScopeRef.Assembly(convertAssemblyReferenceToILAssemblyRef mdReader asmRef)
+
+            | HandleKind.ModuleReference ->
+                let modRef = mdReader.GetModuleReference(ModuleReferenceHandle.op_Explicit(typeRef.ResolutionScope))
+                ILScopeRef.Module(convertModuleReferenceToILModuleRef mdReader modRef)
+
+            | HandleKind.LocalScope ->
+                ILScopeRef.Local
+
+            | _ ->
+                failwithf "Invalid Resolution Scope Handle Kind: %A" typeRef.ResolutionScope.Kind
+
+        let name = mdReader.GetString(typeRef.Name)
+        let namespac = mdReader.GetString(typeRef.Namespace)
+
+        ILTypeRef.Create(ilScopeRef, [], namespac + "." + name)
+
+    let rec convertTypeDefinitionToILTypeRef (mdReader: System.Reflection.Metadata.MetadataReader) (typeDef: TypeDefinition) : ILTypeRef =
+        let enclosing =
+            if typeDef.IsNested then
+                let parentTypeDefHandle = typeDef.GetDeclaringType()
+                let parentTypeDef = mdReader.GetTypeDefinition(parentTypeDefHandle)
+                let ilTypeRef = convertTypeDefinitionToILTypeRef mdReader parentTypeDef
+                ilTypeRef.Enclosing @ [ ilTypeRef.Name ]
+            else
+                []
+      
+        let name =
+            let name = mdReader.GetString(typeDef.Name)
+            if enclosing.Length > 0 then 
+                name
+            else
+                let namespac = mdReader.GetString(typeDef.Namespace)
+                namespac + "." + name
+
+        ILTypeRef.Create(ILScopeRef.Local, enclosing, name)
+
+    let rec convertGenericParameterHandleToILGenericParameterDef (mdReader: System.Reflection.Metadata.MetadataReader) (genParamHandle: GenericParameterHandle) : ILGenericParameterDef =
+        let genParam = mdReader.GetGenericParameter(genParamHandle)
+        let attributes = genParam.Attributes
+
+        let constraints =
+            genParam.GetConstraints()
+            |> Seq.map (fun genParamCnstrHandle ->
+                let genParamCnstr = mdReader.GetGenericParameterConstraint(genParamCnstrHandle)
+                convertEntityHandleToILType mdReader genParamCnstr.Type
+            )
+            |> List.ofSeq     
+
+        let variance = 
+            if int (attributes &&& GenericParameterAttributes.Covariant) <> 0 then
+                ILGenericVariance.CoVariant
+            elif int (attributes &&& GenericParameterAttributes.Contravariant) <> 0 then
+                ILGenericVariance.ContraVariant
+            else
+                ILGenericVariance.NonVariant
+
+        {
+            Name = mdReader.GetString(genParam.Name)
+            Constraints = constraints
+            Variance = variance
+            HasReferenceTypeConstraint = int (attributes &&& GenericParameterAttributes.ReferenceTypeConstraint) <> 0
+            HasNotNullableValueTypeConstraint = int (attributes &&& GenericParameterAttributes.NotNullableValueTypeConstraint) <> 0
+            HasDefaultConstructorConstraint = int (attributes &&& GenericParameterAttributes.DefaultConstructorConstraint) <> 0
+            CustomAttrsStored = storeILCustomAttrs emptyILCustomAttrs // TODO:
+            MetadataIndex = MetadataTokens.GetRowNumber(GenericParameterHandle.op_Implicit(genParamHandle))
+        }
+
+    let rec convertEntityHandleToILType (mdReader: System.Reflection.Metadata.MetadataReader) (handle: EntityHandle) : ILType =
+        match handle.Kind with
+        | HandleKind.TypeReference ->
+            let typeRef = mdReader.GetTypeReference(TypeReferenceHandle.op_Explicit(handle))
+            let ilTypeRef = convertTypeReferenceToILTypeRef mdReader typeRef
+            let ilTypeSpec = ILTypeSpec.Create(ilTypeRef, ILGenericArgs.Empty)
+            mkILTy AsObject (* AsObject probably not nok *) ilTypeSpec
+
+        | HandleKind.TypeDefinition ->
+            let typeDef = mdReader.GetTypeDefinition(TypeDefinitionHandle.op_Explicit(handle))
+            let ilTypeRef = convertTypeDefinitionToILTypeRef mdReader typeDef
+
+            let ilGenericParameterDefs =
+                typeDef.GetGenericParameters()
+                |> Seq.map (convertGenericParameterHandleToILGenericParameterDef mdReader)
+                |> List.ofSeq
+
+            let ilGenericArgs = mkILFormalGenericArgs 0 ilGenericParameterDefs
+
+            let ilTypeSpec = ILTypeSpec.Create(ilTypeRef, ilGenericArgs)
+        
+            let boxity = 
+                if int (typeDef.Attributes &&& TypeAttributes.Class) <> 0 then AsObject
+                else AsValue
+
+            mkILTy boxity ilTypeSpec
+
+        | _ ->
+            failwithf "Invalid Handle Kind: %A" handle.Kind
+
+    let rec convertEntityHandleToILMethodDef (mdReader: System.Reflection.Metadata.MetadataReader) (handle: EntityHandle) : ILMethodSpec =
+        match handle.Kind with
+        | HandleKind.MemberReference ->
+            let memberHandle = MemberReferenceHandle.op_Explicit(handle)
+            let memberRef = mdReader.GetMemberReference(memberHandle)
+        
+            let memberEnclType = convertEntityHandleToILType mdReader memberRef.Parent
+
+            let memberName = mdReader.GetString(memberRef.Name)
+
+            let memberSigHeader, _memberSigTypeCode =
+                let blobReader = mdReader.GetBlobReader(memberRef.Signature)
+                blobReader.ReadSignatureHeader(), blobReader.ReadSignatureTypeCode()
+
+            let _memberCallConv =
+                if memberSigHeader.IsInstance then
+                    ILCallingConv.Instance
+                else
+                    ILCallingConv.Static
+
+            //let memberRetTy =
+            //    memberRef.
+
+           // let _ilMethRef = ILMethodRef.Create(memberEnclType, ILCallingConv.Instance, memberName, 0, [], )
+            //ILMethodSpec.Create(memberEnclType, ILMethodRef.)
+            failwith "not impl"
+        | HandleKind.MethodDefinition ->
+            let methodHandle = MethodDefinitionHandle.op_Explicit(handle)
+            let methodDef = mdReader.GetMethodDefinition(methodHandle)
+
+            let _ilType = convertEntityHandleToILType mdReader (TypeDefinitionHandle.op_Implicit(methodDef.GetDeclaringType()))
+            //ILMethodSpec.Create(ilType,)
+            failwith "not impl"
+        | _ ->
+            failwith "invalid metadata"  
 
 let align alignment n = ((n + alignment - 0x1) / alignment) * alignment
 
@@ -2623,7 +2810,7 @@ and customAttrsReader ctxtH tag : ILAttributesStored =
 and seekReadCustomAttr ctxt (TaggedIndex(cat, idx), b) = 
     ctxt.seekReadCustomAttr (CustomAttrIdx (cat, idx, b))
 
-and seekReadCustomAttrUncached ctxtH (_mdReader: System.Reflection.Metadata.MetadataReader) (CustomAttrIdx (cat, idx, valIdx)) = 
+and seekReadCustomAttrUncached ctxtH (mdReader: System.Reflection.Metadata.MetadataReader) (CustomAttrIdx (cat, idx, valIdx)) = 
     let ctxt = getHole ctxtH
     let method = seekReadCustomAttrType ctxt (TaggedIndex(cat, idx))
     let data =
@@ -2631,63 +2818,21 @@ and seekReadCustomAttrUncached ctxtH (_mdReader: System.Reflection.Metadata.Meta
         | Some bytes -> bytes
         | None -> Bytes.ofInt32Array [| |]
     let elements = []
-    //try
-    //    mdReader.CustomAttributes
-    //    |> Seq.tryFind (fun x ->
-    //        let attr = mdReader.GetCustomAttribute(x)
-    //        let parent =
-    //            match attr.Constructor.Kind with
-    //            | HandleKind.MemberReference ->
-    //                let memberHandle = MemberReferenceHandle.op_Explicit(attr.Constructor)
-    //                let memberDef = mdReader.GetMemberReference(memberHandle)
-    //                memberDef.Parent
-    //            | HandleKind.MethodDefinition ->
-    //                let methodHandle = MethodDefinitionHandle.op_Explicit(attr.Constructor)
-    //                let methodDef = mdReader.GetMethodDefinition(methodHandle)
-    //                TypeDefinitionHandle.op_Implicit(methodDef.GetDeclaringType())
-    //            | _ ->
-    //                failwith "invalid metadata"
+    try
+        let tableIndex =
+            match cat with
+            | x when x = cat_MethodDef -> TableIndex.MethodDef
+            | x when x = cat_MemberRef -> TableIndex.MemberRef
+            | _ -> failwith "Invalid Cat"
+        let handle = MetadataTokens.EntityHandle(tableIndex, idx)
+        let ilMethodDef = ILReader.convertEntityHandleToILMethodDef mdReader handle
+        if method.DeclaringType.TypeRef.FullName.Contains("Beef") || method.DeclaringType.TypeRef.FullName.Contains("Class1") then
+            printfn "%A" method.Name
+            printfn "%A"ilMethodDef
+    with
+    | ex -> 
+        printfn "%A" ex
 
-    //        match parent.Kind with
-    //        | HandleKind.TypeReference ->
-    //            let typeRef = mdReader.GetTypeReference(TypeReferenceHandle.op_Explicit(parent))
-    //            (mdReader.GetString(typeRef.Namespace) + "." + mdReader.GetString(typeRef.Name)).Contains(method.DeclaringType.BasicQualifiedName)
-    //        | HandleKind.TypeDefinition ->
-    //            let typeRef = mdReader.GetTypeDefinition(TypeDefinitionHandle.op_Explicit(parent))
-    //            (mdReader.GetString(typeRef.Namespace) + "." + mdReader.GetString(typeRef.Name)).Contains(method.DeclaringType.BasicQualifiedName)
-    //        | _ -> failwith "invalid metadata"
-    //    )
-    //    |> Option.iter (fun x ->
-    //        printfn "found it: %A" x
-    //        printfn "Real: %i Cat: %i  Idx: %i  ValIdex: %i" (x.GetType().GetField("_rowId", BindingFlags.Instance ||| BindingFlags.NonPublic).GetValue(x) :?> int) cat.Tag idx valIdx
-    //    )
-
-    //    let attrHandle = MetadataTokens.CustomAttributeHandle(idx)
-    //    let attr = mdReader.GetCustomAttribute(attrHandle)
-    
-    //    match attr.Constructor.Kind with
-    //    | HandleKind.MemberReference ->
-    //        let memberHandle = MemberReferenceHandle.op_Explicit(attr.Constructor)
-    //        let memberDef = mdReader.GetMemberReference(memberHandle)
-    //        printfn "%A" (mdReader.GetString(memberDef.Name))
-    //    | HandleKind.MethodDefinition ->
-    //        let methodHandle = MethodDefinitionHandle.op_Explicit(attr.Constructor)
-    //        let methodDef = mdReader.GetMethodDefinition(methodHandle)
-    //        printfn "%A" (mdReader.GetString(methodDef.Name))
-    //    | _ ->
-    //        failwith "invalid metadata"
-
-    //    //let enclTy =
-    //    //    let typeDef = methodDef.GetDeclaringType() |> mdReader.GetTypeDefinition
-    //    //    match mdReader.GetString(typeDef.Name) with
-    //    //    | "void" -> ILType.Void
-    //    //    | _ -> ILType.Void
-    //with
-    //| ex ->
-    //    printfn "%A" ex
-    //    printfn "%A" (method.DeclaringType.BasicQualifiedName)
-    //let method = mkILMethSpecInTy (enclTy, cc, nm, argtys, retty, minst)
-  //  mdReader.GetCustomAttribute(CustomAttributeHandle.op_Explicit(Handle()))
     ILAttribute.Encoded (method, data, elements)
 
 and securityDeclsReader ctxtH tag = 
